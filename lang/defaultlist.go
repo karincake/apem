bf999b5f8610a958be5796bf2a2fae4e0b1ba1f9
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2023-08-08 17:47:45 +0700

    dev: # simplify error # simplify handlerhelper + readme + default lang

---
+++ lang/defaultlist.go
@@ -0,0 +1,8 @@
+package lang
+
+// built-in language for error messages
+var defaultList = langItem{
+	"val-required":        "value is required",
+	"val-integerPositive": "value must be a positive integer",
+	"val-validUuid":       "value must be a valid UUID",
+}
